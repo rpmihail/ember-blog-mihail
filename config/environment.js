62e3423b3eb0182f28ee7666546e784234798c3a
Paul Mihail <rpmihail@valdosta.edu>
2016-11-10 00:11:22 +0000

    awesome first commit

--- config/environment.js
+++ config/environment.js
@@ -12,6 +12,14 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
+    
+    firebase: {         
+      apiKey: "AIzaSyBB-kC7FwRIgZ0F1JQqhUiiODWRMW99vWM",
+      authDomain: "mihail-703e8.firebaseapp.com",
+      databaseURL: "https://mihail-703e8.firebaseio.com",
+      storageBucket: "mihail-703e8.appspot.com",
+      messagingSenderId: "989877082948"    
+    },
 
     APP: {
       // Here you can pass flags/options to your application instance
